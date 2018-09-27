948b9f9d2135354a99e979081724dd9591992b27
gjjuliex <12baysidejgjx@gmail.com>
2018-09-27 09:08:34 -0500

    adding all project files

---
+++ LemonadeStand/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Inventory
+    {
+        public int lemon;
+        public int ice;
+        public int sugar;
+        public int cups;
+        public double budget = 20;
+
+        public Inventory()
+        {
+
+        }
+
+        public void SubtractInventory()
+        {
+            lemon = lemon - 2;
+            ice = ice - 1;
+            sugar = sugar - 1;
+            cups = cups - 2;
+        }
+
+
+
+
+
+
+    }
+}
