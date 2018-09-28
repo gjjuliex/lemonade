99bbb86ed4446df1f43a63f721c9f39a7e1dcd88
gjjuliex <12baysidejgjx@gmail.com>
2018-09-28 08:19:48 -0500

    made the weather class with a random temp and condition

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class Weather
+    public class Weather
     {
-        public int weatherNumber;
-
-
+        public string temperature;
+        public string condition;
 
 
         public Weather()
@@ -20,14 +19,53 @@ namespace LemonadeStand
         }
 
 
-        public int GrabWeather()
+        public int GetTemperature(Random rand)
         {
-            Random random = new random
+            int temperature = rand.Next(1, 4);
+            switch (temperature)
+            {
+                case 1:
+                    this.temperature = rand.Next(51, 60);
+                    break;
+                case 2:
+                    this.temperature = rand.Next(61, 70);
+                    break;
+                case 3:
+                    this.temperature = rand.Next(71, 95);
+                    break;
+                case 4:
+                    this.temperature = rand.Next(96, 110);           
+            }
+            return temperature;
+        }
 
+        public int GetOvercast(Random rand)
+        {
+            int condition = rand.Next(1, 4);
+            switch (condition)
+            {
+                case 1:
+                    this.condition = "cloudy";
+                    break;
+                case 2:
+                    this.condition = "rainy";
+                    break;
+                case 3:
+                    this.condition = "sunny";
+                    break;
+                case 4:
+                    this.condition = "windy";
+                    break;
+            }
+            return condition;
+        }
 
-
+        public void DisplayTodaysWeather()
+        {
+            Console.WriteLine("Today's temperature is {0} and it will be {1)!!", temperature, condition);
         }
 
     }
 
-}
+ }
+
