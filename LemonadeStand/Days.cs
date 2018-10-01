b6baed9932c61eee85e30475ab28eed752397c8a
gjjuliex <12baysidejgjx@gmail.com>
2018-10-01 15:00:16 -0500

    added a days class to make it playable for 7 days

---
+++ LemonadeStand/Days.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Days
+    {
+        public Weather weather;
+        public int day;
+
+
+        public Day()
+        {
+            day = 1;
+            weather = new Weather();
+        }
+
+        public void Weather(Random rand)
+        {
+            weather.GetTemperature(rand);
+            weather.GetOvercast(rand);
+            weather.DisplayTodaysWeather();
+        }
+
+        public void DisplayDay()
+        {
+            Console.WriteLine("Day {1}", day);
+        }
+
+
+    }   
+
+}
+
+
